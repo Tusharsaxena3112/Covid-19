5e65e5a9ff459f25211fb6fb7730fb7033451ffb
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-09-02 12:41:26 +0530

    staged changes

--- Front-end/js/video-speed.js
+++
@@ -1,7 +0,0 @@
-var about_video   = document.getElementById('about-video')
-
-about_video.defaultPlaybackRate = 10.0;
-
-var vid = document.getElementById('myVideo')
-
-vid.defaultPlaybackRate = 10.0;
---
+++ static/js/video-speed.js
@@ -0,0 +1,7 @@
+var about_video = document.getElementById('about-video');
+
+about_video.defaultPlaybackRate = 10.0;
+
+var vid = document.getElementById('myVideo');
+
+vid.defaultPlaybackRate = 10.0;
