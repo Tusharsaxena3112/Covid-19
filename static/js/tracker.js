779ecdc5ce5b262025f47fa45d43cc1a1843af4e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-09-06 17:42:32 +0530

    getting data

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -23,5 +23,34 @@ country_list.forEach(country => {
     if (country.code == countryCode) {
         userCountry = country.name;
     }
-    console.log(userCountry)
 });
+
+// function fetchData(userCountry)
+// {
+//     // fetch('https://covid19.mathdro.id/api/', {
+// 	// 	"method": "GET",
+// 	// 	// "headers": {
+// 	// 	// 	"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
+// 	// 	// 	"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
+// 	// 	// }
+// 	// }).then(
+//     //     response=>{
+//     //         console.log(response.json())}
+//     // ).then(data=>{
+//     //     console.log(data);
+//     // })
+   
+// }
+async function fetchData(userCountry) {
+    let url = `https://covid19.mathdro.id/api/countries/${userCountry}`;
+    await fetch(url)
+    .then(res=>{
+        return res.json();
+    })
+    .then(data=>{
+        
+    })
+}
+
+fetchData(userCountry);
+
