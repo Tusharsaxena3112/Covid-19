e34ee82079e9104e55971726d3a659635f84f3b1
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-09-06 14:31:37 +0530

    creating tracker.js

---
+++ static/js/tracker.js
@@ -0,0 +1,8 @@
+let country = document.querySelector('.country name')
+let totalcases = document.querySelector('.total-cases .value')
+let newCases = document.querySelector('.total-cases .new-value')
+let recovered = document.querySelector('.recovered-cases value')
+let newRecovered = document.querySelector('.recovered-cases .new-value')
+let totaldeaths = document.querySelector('.total-deaths .value')
+let newDeaths = document.querySelector('.total-deaths .new-value')
+
