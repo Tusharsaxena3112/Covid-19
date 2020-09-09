772fed2982c1de64eb94c98c8f1193b49c09b2a1
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-09-09 11:37:01 +0530

    add function to get the data from api

--- static/js/news.js
+++ static/js/news.js
@@ -1,1 +1,13 @@
 const api_url = "https://cryptic-ravine-96718.herokuapp.com/";
+
+async function getapi(url){
+    const response = await fetch(url);
+
+    var data = await response.json();
+    console.log(data);
+    if(response){
+        hideloader();
+    }
+    show(data);
+}
+
