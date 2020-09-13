8698a5a6854efbf92de40b261ea6b17a90703292
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-09-13 19:16:14 +0530

    working tracker.js

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -1,4 +1,4 @@
-let appData = [];
+let appData = [],countryData=[];
 function fetchWorldData() {
   let url = `https://covid19.mathdro.id/api/`;
   fetch(url)
@@ -56,19 +56,64 @@ async function fetchCountryData(userCountry) {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      countryData.push(data["confirmed"]["value"]);
+      countryData.push(data["recovered"]["value"]);
+      countryData.push(data["deaths"]["value"]);
       appendCountryData(data);
     });
 }
 fetchCountryData(userCountry);
 
 function appendCountryData(data) {
-  var country_total_cases = document.querySelector('.total-cases .value');
-  var country_recovered_cases = document.querySelector('.recovered-cases .value');
-  var country_deaths = document.querySelector('.total-deaths .value');
+  var country_total_cases = document.querySelector(".total-cases .value");
+  var country_recovered_cases = document.querySelector(
+    ".recovered-cases .value"
+  );
+  var country_deaths = document.querySelector(".total-deaths .value");
   var countryElement = document.querySelector(".country .name");
   countryElement.innerHTML = userCountry;
-  country_total_cases.innerHTML = data['confirmed']['value'];
-  country_recovered_cases.innerHTML = data['recovered']['value'];
-  country_deaths.innerHTML = data['deaths']['value'];
+  country_total_cases.innerHTML = data["confirmed"]["value"];
+  country_recovered_cases.innerHTML = data["recovered"]["value"];
+  country_deaths.innerHTML = data["deaths"]["value"];
 }
+
+// const bar = document.getElementById("axes-line-chart").getContext("2d");
+//   let bar_chart = new Chart(bar, {
+//     type: 'bar',
+//     data: {
+//       labels:['Confirmed','Recovered','Deaths'],
+//       datasets: [
+//         {
+//           data: countryData,
+//         },
+//       ],
+//     },
+//     options = {
+//       scales: {
+//           xAxes: [{
+//               gridLines: {
+//                   offsetGridLines: true
+//               }
+//           }]
+//       }
+//   }
+// });
+
+const bar = document.getElementById("axes-line-chart").getContext("2d");
+let myCountryChart = new Chart(bar, {
+  type: "pie",
+  data: {
+    labels: ["Confirmed", "Recovered", "Deaths"],
+    datasets: [
+      {
+        label: "",
+        data: countryData,
+        backgroundColor: ["#311cfd", "#31fb2a", "#fb0c2a"],
+        borderWidth: 3,
+      },
+    ],
+  },
+});
+
+
+
