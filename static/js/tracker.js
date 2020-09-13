d1e959c12063b75dd28426e34e6abf474175332d
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-09-13 21:19:37 +0530

    cleaning the code base

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -1,6 +1,7 @@
-let appData = [],countryData=[];
+let appData = [],
+  countryData = [];
 function fetchWorldData() {
-  appData=[];
+  appData = [];
   let url = `https://covid19.mathdro.id/api/`;
   fetch(url)
     .then((res) => {
@@ -26,40 +27,46 @@ function appendData(data) {
 fetchWorldData();
 
 //Adding Pie Chart
-function createPieChart(){
+function createPieChart() {
   const ctx = document.getElementById("chart-container").getContext("2d");
-let myChart = new Chart(ctx, {
-  type: "pie",
-  data: {
-    labels: ["Confirmed", "Recovered", "Deaths"],
-    datasets: [
-      {
-        label: "",
-        data: appData,
-        backgroundColor: ["#311cfd", "#31fb2a", "#fb0c2a"],
-        borderWidth: 3,
-      },
-    ],
-  },
-});
+  let myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Confirmed", "Recovered", "Deaths"],
+      datasets: [
+        {
+          label: "",
+          data: appData,
+          backgroundColor: ["#311cfd", "#31fb2a", "#fb0c2a"],
+          borderWidth: 3,
+        },
+      ],
+    },
+  });
 
-const worldBar = document.getElementById("bar-chart-container").getContext("2d");
-let myWorldBarChart = new Chart(worldBar, {
-  type: 'bar',
-  data: {
-    labels: ["Confirmed", "Recovered", "Deaths"],
-    datasets: [
-      {label:'World Statistics',
-        data: appData,
-        borderWidth:'1',
-        borderColor:'black',
-        backgroundColor:['rgb(14, 94, 253,0.4)','rgb(3, 249, 104,0.4)','rgb(252, 94, 104,0.4)']
-      },
-    ],
-  },
-});
+  const worldBar = document
+    .getElementById("bar-chart-container")
+    .getContext("2d");
+  let myWorldBarChart = new Chart(worldBar, {
+    type: "bar",
+    data: {
+      labels: ["Confirmed", "Recovered", "Deaths"],
+      datasets: [
+        {
+          label: "World Statistics",
+          data: appData,
+          borderWidth: "1",
+          borderColor: "black",
+          backgroundColor: [
+            "rgb(14, 94, 253,0.4)",
+            "rgb(3, 249, 104,0.4)",
+            "rgb(252, 94, 104,0.4)",
+          ],
+        },
+      ],
+    },
+  });
 }
-
 
 //   Getting Location of User
 let countryCode = geoplugin_countryCode();
@@ -71,7 +78,7 @@ country_list.forEach((country) => {
 });
 
 async function fetchCountryData(userCountry) {
-  countryData = []
+  countryData = [];
   let url = `https://covid19.mathdro.id/api/countries/${userCountry}`;
   await fetch(url)
     .then((res) => {
@@ -82,12 +89,12 @@ async function fetchCountryData(userCountry) {
       countryData.push(data["recovered"]["value"]);
       countryData.push(data["deaths"]["value"]);
       createCountryChart();
-      appendCountryData(userCountry,data);
+      appendCountryData(userCountry, data);
     });
 }
 fetchCountryData(userCountry);
 
-function appendCountryData(userCountry,data) {
+function appendCountryData(userCountry, data) {
   var country_total_cases = document.querySelector(".total-cases .value");
   var country_recovered_cases = document.querySelector(
     ".recovered-cases .value"
@@ -100,38 +107,41 @@ function appendCountryData(userCountry,data) {
   country_deaths.innerHTML = data["deaths"]["value"];
 }
 
-function createCountryChart(){
-const line = document.getElementById("line-chart").getContext("2d");
-let myCountrylineChart = new Chart(line, {
-  type: 'line',
-  data: {
-    labels: ["Confirmed", "Recovered", "Deaths"],
-    datasets: [
-      {label:'Country Data',
-        data: countryData,
-        borderColor:'black',
-        borderWidth:'0.5',
-        backgroundColor:'rgb(253, 254, 0,0.3)'
-      },
-    ],
-  },
-});
-const bar = document.getElementById("bar-chart").getContext("2d");
-let myCountryBarChart = new Chart(bar, {
-  type: 'bar',
-  data: {
-    labels: ["Confirmed", "Recovered", "Deaths"],
-    datasets: [
-      {label:'Country Data',
-        data: countryData,
-        borderWidth:'1',
-        borderColor:'black',
-        backgroundColor:['rgb(14, 94, 253,0.4)','rgb(3, 249, 104,0.4)','rgb(252, 94, 104,0.4)']
-      },
-    ],
-  },
-});
+function createCountryChart() {
+  const line = document.getElementById("line-chart").getContext("2d");
+  let myCountrylineChart = new Chart(line, {
+    type: "line",
+    data: {
+      labels: ["Confirmed", "Recovered", "Deaths"],
+      datasets: [
+        {
+          label: "Country Data",
+          data: countryData,
+          borderColor: "black",
+          borderWidth: "0.5",
+          backgroundColor: "rgb(253, 254, 0,0.3)",
+        },
+      ],
+    },
+  });
+  const bar = document.getElementById("bar-chart").getContext("2d");
+  let myCountryBarChart = new Chart(bar, {
+    type: "bar",
+    data: {
+      labels: ["Confirmed", "Recovered", "Deaths"],
+      datasets: [
+        {
+          label: "Country Data",
+          data: countryData,
+          borderWidth: "1",
+          borderColor: "black",
+          backgroundColor: [
+            "rgb(14, 94, 253,0.4)",
+            "rgb(3, 249, 104,0.4)",
+            "rgb(252, 94, 104,0.4)",
+          ],
+        },
+      ],
+    },
+  });
 }
-
-
-
