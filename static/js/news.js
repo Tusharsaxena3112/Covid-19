ab955a3ad0f78b49c7c42cf4033b94d46016e66b
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-09-14 21:16:49 +0530

    replace positions

--- static/js/news.js
+++ static/js/news.js
@@ -22,8 +22,8 @@ function appendData(data){
     data.forEach(element => {
         newsElement.innerHTML += `
         <div class='news-item'>
-        <div class='news-title'>${element.title}</div>
         <div class='news-img'><img src='${element.img}' width="500px"></div>
+        <div class='news-title'>${element.title}</div>
         <div class='news-link'><a href="${element.link}" target="_blank" rel="noopener noreferrer">For Detailed Description...</a></div>
         </div>`
      });
