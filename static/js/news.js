8b17878a8422fbeff3a813a5530315c6a0f5da06
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-10-15 00:27:02 +0530

    fixing readmore button

--- static/js/news.js
+++ static/js/news.js
@@ -29,7 +29,7 @@ function appendData(data){
         newsElement.innerHTML += `
         <div class='news-item'>
         <div class='news-img'><img src='${element.img}' width="500px"/></div>
-        <div class='news-title'>${element.title}....</div>
+        <div class='news-title'>${element.title}....                                              </div>
         <div class='news-link'><a href="${element.link}" target="_blank" rel="noopener noreferrer">Read More</a></div>
         </div>`
     });
