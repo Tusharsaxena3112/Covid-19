b128f4166e8dbff1d8711c57928993aea22e28d3
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-10-17 19:30:05 +0530

    adding data

--- static/js/medicare.js
+++ static/js/medicare.js
@@ -1,8 +1,29 @@
-let url = "https://api.rootnet.in/covid19-in/hospitals/medical-colleges"
+let url = "https://api.rootnet.in/covid19-in/hospitals/medical-colleges";
+
 fetch(url)
 .then((res)=>{
     return res.json();
 })
 .then(data=>{
-    console.log(data)
+    console.log(data.data.medicalColleges);
+    add_data(data.data.medicalColleges);
+    filter_data(data.data.medicalColleges);
 })
+ function filter_data(){
+
+ }
+
+function add_data(hospitals){
+    var h = document.getElementById('hospital_section');
+    hospitals.forEach(element => {
+       h.innerHTML+= `
+        <div class='hospital'>
+            // <img src='../static/images/doctor.jpg'/>
+            <h3 class='name'>${element.name}</h3>
+            <h4 class='beds'>${element.hospitalBeds}</h4>
+            <h4 class='state'>${element.state}</h4>
+        </div>
+       `
+    });
+
+}
