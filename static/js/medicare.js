bfac33f779086c0ed92af0c4f02275ba5d985e22
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-10-17 23:19:11 +0530

    almost done

--- static/js/medicare.js
+++ static/js/medicare.js
@@ -1,29 +1,47 @@
 let url = "https://api.rootnet.in/covid19-in/hospitals/medical-colleges";
-
+var d = [];
 fetch(url)
-.then((res)=>{
+  .then((res) => {
     return res.json();
-})
-.then(data=>{
-    console.log(data.data.medicalColleges);
+  })
+  .then((data) => {
+    data.data.medicalColleges.forEach(el=>{
+        d.push(el);
+    })
     add_data(data.data.medicalColleges);
-    filter_data(data.data.medicalColleges);
-})
- function filter_data(){
+  });
 
- }
-
-function add_data(hospitals){
-    var h = document.getElementById('hospital_section');
-    hospitals.forEach(element => {
-       h.innerHTML+= `
+function filter_add() {
+  var h = document.getElementById("hospital_section");
+  var search_box = document.getElementById("hospital_id");
+  var value = search_box.value;
+  if (value!=' ') {
+    h.innerHTML = '';
+    d.forEach((element) => {
+      if (value.toLowerCase() === element.city.toLowerCase()) {
+        h.innerHTML += `
         <div class='hospital'>
             <h3 class='name'>${element.name}</h3>
             <h4 class='beds'>Beds:${element.hospitalBeds}</h4>
             <h4 class='beds'>City:${element.city}</h4>
             <h4 class='state'>${element.state}</h4>
         </div>
-       `
+       `;
+      }
     });
+  }
+}
 
+function add_data() {
+  var h = document.getElementById("hospital_section");
+  d.forEach((element) => {
+    h.innerHTML += `
+        <div class='hospital'>
+            <h3 class='name'>${element.name}</h3>
+            <h4 class='beds'>Beds:${element.hospitalBeds}</h4>
+            <h4 class='beds'>City:${element.city}</h4>
+            <h4 class='state'>${element.state}</h4>
+        </div>
+       `;
+  });
 }
