216b1b6469c564145cb07b8085d6e9d1c900c5b1
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-10-28 09:05:23 +0530

    adding route for contact

--- app.py
+++ app.py
@@ -28,5 +28,10 @@ def medi_care():
     return render_template('medicare.html')
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
